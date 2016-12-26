ebdb8151e021c5e00a5666a610164ec18ec1dd77
loustler <dev.loustler@gmail.com>
2016-12-26 18:20:27 +0900

    refactor : improve traverse function
    - Finish function if parameter 1, 2 is null or empty string
    - Finish function if command not exist
    - Look like not move current path(move original path when execute rtd function)

--- shell/func/traverse.sh
+++ shell/func/traverse.sh
@@ -1,7 +1,23 @@
+
+
 function rtd {
 local MY_CMD="$1"
 local MY_PATH="$2"
 local MY_DEPTH="$3"
+local CNT=0
+local MY_CURRENT_PATH=$(pwd)
+
+if [ -z $MY_PATH -o -z $MY_CMD ]
+  then
+    echo "First parameter is command execute you want.\r\nSecond paramter is directory path execute to command."
+    return 1
+fi
+
+if [[ "$(which $MY_CMD)" == *"not found" ]]
+  then
+    echo "$MY_CMD not found. Check this function if exist."
+    return 1
+fi
 
 if ($MY_DEPTH)
 then MY_DEPTH=1
@@ -9,18 +25,19 @@ fi
 
 echo "input command is '$MY_CMD' start!"
 
-local CNT=0
 while IFS= read -r -d $'\0'; do
-  if ((CNT >= 1))
+  if [ "$MY_PATH" != "$REPLY" ]
     then
-    echo "$CNT----------------------------------------"
-    echo "starting $MY_CMD in $REPLY"
-    cd "$REPLY"
-    eval $MY_CMD
-    echo "----------------------------------------$CNT"
+      echo "$CNT----------------------------------------"
+      echo "starting $MY_CMD in $REPLY"
+      cd "$REPLY"
+      eval $MY_CMD
+      echo "----------------------------------------$CNT"
   fi
-  CNT=$(($CNT+1))
-done < <(find $MY_PATH -type d -maxdepth $MY_DEPTH -print0)
+  CNT=$(($CNT + 1))
+done < <(find $MY_PATH -type d -maxdepth $MY_DEPTH -print0) 2>/dev/null
+
+cd "$MY_CURRENT_PATH"
 
 echo "finish \r\nCreate by loustler(https://github.com/loustler/my-setting)"
 }
