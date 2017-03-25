48d45f12dbfda6283fa30791f532dd690a38369b
loustler <dev.loustler@gmail.com>
2017-03-25 14:55:08 +0900

    Add useful function for make directory like java and scala structure.

--- shell/func/utilities.sh
+++ shell/func/utilities.sh
@@ -2,3 +2,25 @@ function attach_conf {
   source ~/.bash_profile
   source ~/.zshrc
 }
+
+function mkDirSbt {
+ local PATH=$1
+ 
+ if [ -z $PATH ]
+ then
+	 PATH = "$(pwd)"
+ fi
+
+ mkdir -p "$PATH/src/{main,test}/{java,resources/scala}"
+}
+
+function mkDirJava {
+ local PATH=$1
+
+ if [ -z $PATH ]
+ then
+	 PATH = "$(pwd)"
+ f
+
+ mkdir -p "$PATH/src/{main,test}/{java,resources}"
+}
